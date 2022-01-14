13ca4f7ec1bca16eadcbd0a00eaa0fde796211e8
berry <bernhard.hustomo@gmail.com>
2022-01-14 16:51:33 +0800

    just trying to make things right
    just trying to make things work properly

---
+++ src/HelloWorldTwelve.java
@@ -0,0 +1,6 @@
+public class HelloWorldTwelve {
+    public static void main(String[] args) {
+        System.out.println("Hello World Twelve");
+        System.out.println("Let's Play Hades");
+    }
+}
